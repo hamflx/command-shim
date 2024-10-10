63c43ff085b8ce0b2e536f3eb5a53c4009b87f15
hamflx <a@hamflx.cn>
2024-10-10 01:57:52 +0000

    add linux support

--- crates/generate/src/main.rs
+++ crates/generate/src/main.rs
@@ -1,12 +1,15 @@
-use libsui::PortableExecutable;
-
 fn main() {
     let command = std::env::args().skip(1).next().unwrap();
 
     let exe_binary = include_bytes!(env!("CARGO_BIN_FILE_SHIM_shim"));
     let exe_binary = exe_binary.to_vec();
     let mut output = Vec::new();
-    PortableExecutable::from(&exe_binary)
+    #[cfg(unix)]
+    libsui::Elf::new(&exe_binary)
+        .append("command", command.as_bytes(), &mut output)
+        .unwrap();
+    #[cfg(windows)]
+    libsui::PortableExecutable::from(&exe_binary)
         .unwrap()
         .write_resource("command", command.as_bytes().into())
         .unwrap()
